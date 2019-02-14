fefaff46908803214d0966e6a723e5b9514d8a5d
mitchya1 <mitchell.anderson@colorado.edu>
2019-02-14 09:57:28 -0700

    add second arg to post func

--- omsa-scripts/post_metrics.py
+++ omsa-scripts/post_metrics.py
@@ -1,9 +1,9 @@
 import requests, json
 from config import *
-class PostMetrics:
+class PostMetrics():
 
     headers = {'Content-Type': 'application/json'}
-    def post(json_report):
+    def post(self, json_report):
         if verify_ssl:
 
             r = requests.post(api_server, headers=headers, data=json.dumps(json_report))
